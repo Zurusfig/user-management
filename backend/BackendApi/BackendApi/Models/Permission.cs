205eb6bdbc34b4285390331aebcd66d855f2629f
Zagif <zagif2151382@gmail.com>
2026-02-24 23:51:32 +0700

    feat: define models for User, Role, Permission and UserPermission

---
+++ backend/BackendApi/BackendApi/Models/Permission.cs
@@ -0,0 +1,13 @@
+﻿namespace BackendApi.Models
+{
+    public class Permission
+    {
+        public string Id { get; set; } = null!;
+        public string PermissionId { get; set; } = null!;
+        public bool IsReadable { get; set; }
+        public bool IsWritable { get; set; }
+        public bool IsDeletable { get; set; }
+        public string UserId { get; set; } = null!; //Foreign key to User
+
+    }
+}
