1bafe17ab1db4a037c71dfc0942c6dc67fdadb59
Zagif <zagif2151382@gmail.com>
2026-02-25 22:29:45 +0700

    feat: simplify Permission model

--- backend/BackendApi/BackendApi/Models/Permission.cs
+++ backend/BackendApi/BackendApi/Models/Permission.cs
@@ -2,12 +2,7 @@
 {
     public class Permission
     {
-        public string Id { get; set; } = null!;
-        public string PermissionId { get; set; } = null!;
-        public bool IsReadable { get; set; }
-        public bool IsWritable { get; set; }
-        public bool IsDeletable { get; set; }
-        public string UserId { get; set; } = null!; //Foreign key to User
-
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = null!;
     }
 }
