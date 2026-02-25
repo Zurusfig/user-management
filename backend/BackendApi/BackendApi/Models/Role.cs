76bf88955bcfb74edd2cd28533339045746ee2d3
Zagif <zagif2151382@gmail.com>
2026-02-25 22:28:28 +0700

    feat: define models for Role and UserPermission

--- backend/BackendApi/BackendApi/Models/Role.cs
+++ backend/BackendApi/BackendApi/Models/Role.cs
@@ -2,5 +2,7 @@
 {
     public class Role
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = null!;
     }
 }
--- backend/BackendApi/BackendApi/Models/UserPermission.cs
+++ backend/BackendApi/BackendApi/Models/UserPermission.cs
@@ -2,5 +2,11 @@
 {
     public class UserPermission
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public bool IsReadable { get; set; }
+        public bool IsWritable { get; set; }
+        public bool IsDeletable { get; set; }
+        public string UserId { get; set; } = null!; //Foreign key to User
+        public string PermissionId { get; set; } = null!; //Foreign key to Permission
     }
 }
