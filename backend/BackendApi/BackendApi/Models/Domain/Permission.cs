6d945d9c9a858d546c6ab52af0ea2bb848367378
Zagif <zagif2151382@gmail.com>
2026-02-25 23:47:11 +0700

    fix: change Id of User, Role, Permission to be manual required input

--- backend/BackendApi/BackendApi/Models/Domain/Permission.cs
+++ backend/BackendApi/BackendApi/Models/Domain/Permission.cs
@@ -2,7 +2,7 @@
 {
     public class Permission
     {
-        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Id { get; set; } = null!;
         public string Name { get; set; } = null!;
     }
 }
