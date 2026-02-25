a472f4097717845927898ba6a45bdb3596e90620
Zagif <zagif2151382@gmail.com>
2026-02-25 22:54:43 +0700

    feat: define DbContext Class

---
+++ backend/BackendApi/BackendApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BackendApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<UserPermission> UserPermissions { get; set; }
+    }
+}
