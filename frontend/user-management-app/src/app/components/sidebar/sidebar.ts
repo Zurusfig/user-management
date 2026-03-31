320db84d28de5670c671d5bbc441513bc31ba33c
Zagif <zagif2151382@gmail.com>
2026-03-31 23:30:02 +0700

    feat: add sidebar elements and style

--- frontend/user-management-app/src/app/components/sidebar/sidebar.ts
+++ frontend/user-management-app/src/app/components/sidebar/sidebar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
