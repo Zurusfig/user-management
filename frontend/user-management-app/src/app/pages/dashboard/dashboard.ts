c71fb696921f6722f0b7df4c18340a0cd804b96b
Zagif <zagif2151382@gmail.com>
2026-04-04 20:35:53 +0700

    feat: initialize dashboard page components and user-table

--- frontend/user-management-app/src/app/pages/dashboard/dashboard.ts
+++ frontend/user-management-app/src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { UserTable } from "../../components/user-table/user-table";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [UserTable, MatIconModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
