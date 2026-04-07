9f200b59cd41b7a970de2f66ae3b19eb42de852c
Zagif <zagif2151382@gmail.com>
2026-04-07 08:56:01 +0700

    feat: initialize Pagination component and UI

---
+++ frontend/user-management-app/src/app/components/pagination/pagination.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-pagination',
+  imports: [MatIconModule],
+  templateUrl: './pagination.html',
+  styleUrl: './pagination.scss',
+})
+export class Pagination {
+
+}
