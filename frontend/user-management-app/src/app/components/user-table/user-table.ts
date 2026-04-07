72650e2f5c67d9809b09f8338fffca135b312033
Zagif <zagif2151382@gmail.com>
2026-04-07 08:18:55 +0700

    feat: implement User Table

--- frontend/user-management-app/src/app/components/user-table/user-table.ts
+++ frontend/user-management-app/src/app/components/user-table/user-table.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { UserService } from '../../services/user-service';
+import { DataTableRequest } from '../../models/user.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-table',
-  imports: [MatIconModule],
+  imports: [MatIconModule, DatePipe],
   templateUrl: './user-table.html',
   styleUrl: './user-table.scss',
 })
-export class UserTable {
+export class UserTable implements OnInit{
+  private dataTableService = inject(UserService);
 
+  // Place holder for Pagination
+  orderBy = null;
+  orderDirection = null;
+  pageNumber = 1;
+  pageSize = 1;
+  search = null;
+  dataTableRequest: DataTableRequest = {
+  orderBy: undefined,
+  orderDirection: undefined,
+  pageNumber: 1,
+  pageSize: 1,
+  search: undefined
+};
+
+  ngOnInit() {
+    this.dataTableService.getUsers(this.dataTableRequest);
+  }
+
+  isLoading = this.dataTableService.isLoading;
+  isError = this.dataTableService.isError;
+  userList = computed(() => this.dataTableService.users()?.dataSource ?? []);
 }
