8f404ae9dbd5a3cb9543fddf69b05bc0d7ebad1f
Zagif <zagif2151382@gmail.com>
2026-04-07 08:56:29 +0700

    feat: add pagination to dashboard page and edit styles

--- frontend/user-management-app/src/app/pages/dashboard/dashboard.ts
+++ frontend/user-management-app/src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserTable } from "../../components/user-table/user-table";
 import { MatIconModule } from '@angular/material/icon';
+import { Pagination } from '../../components/pagination/pagination';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [UserTable, MatIconModule],
+  imports: [UserTable, MatIconModule, Pagination],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
