46c6dcd87d5444e11b438159574ac9202a0908a6
Zagif <zagif2151382@gmail.com>
2026-04-18 18:07:29 +0700

    fix: fix httpResource response data type to ApiResponse

--- frontend/user-management-app/src/app/services/permission-service.ts
+++ frontend/user-management-app/src/app/services/permission-service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { httpResource } from '@angular/common/http';
 import { Permission } from '../models/permission.model';
+import { ApiResponse } from '../models/api-response.model';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -9,8 +11,8 @@ export class PermissionService {
   private apiBaseUrl = environment.apiBaseUrl;
 
   getAllPermissions() {
-    return httpResource<Permission[]>(() =>
+    return httpResource<ApiResponse<Permission[]>>(() =>
       `${this.apiBaseUrl}/api/Permissions`)
   }
-  
+
 }
--- frontend/user-management-app/src/app/services/role-service.ts
+++ frontend/user-management-app/src/app/services/role-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { httpResource } from '@angular/common/http';
 import { Role } from '../models/role.model';
+import { ApiResponse } from '../models/api-response.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ export class RoleService {
   private apiBaseUrl = environment.apiBaseUrl;
 
   getAllRoles() {
-    return httpResource<Role[]>(() =>
+    return httpResource<ApiResponse<Role[]>>(() =>
       `${this.apiBaseUrl}/api/Roles`)
   }
 
