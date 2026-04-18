9b2394dbe4d2ddc7d3e34b0f6aa29b36dd4c20f6
Zagif <zagif2151382@gmail.com>
2026-04-18 18:07:50 +0700

    feat: connect Roles and Permission to AddUserModal

--- frontend/user-management-app/src/app/components/add-user-modal/add-user-modal.ts
+++ frontend/user-management-app/src/app/components/add-user-modal/add-user-modal.ts
@@ -1,5 +1,7 @@
-import { Component, output } from '@angular/core';
+import { Component, computed, inject, output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { PermissionService } from '../../services/permission-service';
+import { RoleService } from '../../services/role-service';
 
 @Component({
   selector: 'app-add-user-modal',
@@ -9,4 +11,25 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class AddUserModal {
   closeModal = output<void>();
+  private roleService = inject(RoleService);
+  private permissionService = inject(PermissionService);
+
+  private getAllRolesRef = this.roleService.getAllRoles();
+  private getAllPermissionsRef = this.permissionService.getAllPermissions();
+
+  roleIsLoading = this.getAllRolesRef.isLoading;
+  roleIsError = this.getAllRolesRef.error;
+  roles = computed(() => this.getAllRolesRef.value()?.data ?? []);
+
+  rolesDebug = computed(() => {
+    console.log('roles loading:', this.getAllRolesRef.isLoading());
+    console.log('roles error:', this.getAllRolesRef.error());
+    console.log('roles value:', this.getAllRolesRef.value());
+    return this.getAllRolesRef.value();
+  });
+
+  permissionIsLoading = this.getAllPermissionsRef.isLoading;
+  permissionIsError = this.getAllPermissionsRef.error;
+  permissions = this.getAllPermissionsRef.value;
+
 }
